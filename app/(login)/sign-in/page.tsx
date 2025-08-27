f29dbb5b91bf866caf2f795be2263f9d756cb22e
AljazMusic11 <63785933+AljazMusic11@users.noreply.github.com>
2025-08-27 19:20:51 +0200

    Update page.tsx

--- app/(login)/sign-in/page.tsx
+++ app/(login)/sign-in/page.tsx
@@ -3,7 +3,7 @@ export const runtime = 'nodejs';
 import { Suspense } from 'react';
 import { Login } from '../login';
 
-export default function SignInPage({ searchParams }: { searchParams?: { [k: string]: string } }) {
+export default function SignInPage({ searchParams }: { searchParams?: Record<string, string> }) {
   return (
     <Suspense>
       <Login mode="signin" searchParams={searchParams as any} />
