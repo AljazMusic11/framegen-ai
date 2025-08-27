f22b5d99dddd383f323b989b2219609564606653
AljazMusic11 <63785933+AljazMusic11@users.noreply.github.com>
2025-08-27 19:04:25 +0200

    Update page.tsx

--- app/(login)/sign-up/page.tsx
+++ app/(login)/sign-up/page.tsx
@@ -3,10 +3,10 @@ export const runtime = 'nodejs';
 import { Suspense } from 'react';
 import { Login } from '../login';
 
-export default function SignUpPage() {
+export default function SignUpPage({ searchParams }: { searchParams?: { [k: string]: string } }) {
   return (
     <Suspense>
-      <Login mode="signup" />
+      <Login mode="signup" searchParams={searchParams as any} />
     </Suspense>
   );
 }
